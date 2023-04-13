bbfadf654fc1e6b08ece47ee4eb74f45f5f2a33f
HadiHesham <125379428+HadiHesham@users.noreply.github.com>
2023-04-13 09:27:31 -0700

    Update and rename main.cpp to Q2main.cpp

--- Q2main.cpp
+++ Q2main.cpp
@@ -4,6 +4,14 @@
 using namespace std;
 int main(){
     linkedlist l,t;
+    l.Add(2,-1,0);
+    l.Add(5,-2,3);
+    l.Add(7,-1,6);
+    l.Add(6, 3,1);
+    l.Print();
+    l.Delete(6);
+    l.Delete(2);
+    l.Print();
     vector<int> v{4,4,5,6,4,5,6,7,5};
     t=l.vectorb(v);
     t.Print();
