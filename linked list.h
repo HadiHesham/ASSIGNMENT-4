c8ce587ab0f63dd0a4b2703e2f2e8dae22c2b695
HadiHesham <125379428+HadiHesham@users.noreply.github.com>
2023-04-13 12:36:16 -0700

    Update linked list.h

--- linked list.h
+++ linked list.h
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+// Define a node structure to store value, occurrence and pointer to next node
 struct node{
     int value;
     int occorunce;
     node *next;
 };
+
+// Define a linked list class
 class linkedlist{
 private:
-    node *h;
+    node *h; // Declare head node pointer
 public:
-    linkedlist();
-    void Add(int,int,int);
-    void Delete(int);
-    void Print();
-    linkedlist vectorb(vector<int>);
-    int sum();
+    linkedlist(); // Constructor
+    void Add(int,int,int); // Add node to the list
+    void Delete(int); // Delete node from the list
+    void Print(); // Print the list
+    linkedlist vectorb(vector<int>); // Create a linked list from a vector
+    int sum(); // Calculate the sum of all the nodes in the list
 };
+
